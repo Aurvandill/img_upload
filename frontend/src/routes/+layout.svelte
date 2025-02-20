7a67770030ff364d2351ac6d5c9a646f77d103ef
Aurvandill <cerberus885@gmail.com>
2025-02-21 00:39:55 +0100

    frontend anpassungen

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,11 +1,21 @@
 <script>
 	import 'bootstrap/scss/bootstrap.scss';
 	import 'bootstrap-icons/font/bootstrap-icons.css';
+	import ToastContainer from '$lib/components/toast_container.svelte';
 
 	let { children } = $props();
-	import NavBar from '$lib/nav_bar.svelte';
+	import NavBar from '$lib/components/nav_bar.svelte';
 	import '../app.css';
 </script>
 
 <NavBar />
+<div class="content">
 {@render children()}
+</div>
+
+<ToastContainer />
+<style>
+	.content{
+		max-width: 100vw;
+	}
+</style>
--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -1,40 +1,54 @@
 <script>
 	import { API_URL } from '$lib/defines';
+	import { addToast, authenticated } from '$lib/stores';
+	import { goto } from '$app/navigation';
 
 	async function login() {
-		console.log(username, password);
 		const url = `${API_URL}/Session/login`;
 		try {
 			const response = await fetch(url, {
-                method: "POST",
-                body: JSON.stringify({"username":username, "password":password})
-            });
+				method: 'POST',
+				body: JSON.stringify({ username: username, password: pw })
+			});
 			if (!response.ok) {
+				let json = { msg: 'unbekannter Fehler' };
+				try {
+					json = await response.json();
+				} catch (error) {
+					json = { msg: 'unbekannter Fehler' };
+				}
+
+				addToast({ message: json.msg, type: 'danger', heading: 'Fehler!' });
 				throw new Error(`Response status: ${response.status}`);
 			}
 
 			const json = await response.json();
-			console.log(json);
+			addToast({ message: json.msg, type: 'primary' });
+			$authenticated = true;
+			goto('/');
 		} catch (error) {
 			console.error(error.message);
 		}
 	}
 
-	let username = '';
-	let password = '';
+	let username = $state('');
+	let pw = $state('');
 </script>
 
-<div class="row d-flex justify-content-center">
+<div class="d-flex justify-content-center">
 	<div class="col-md-8 col-lg-6 col-xl-4">
-		<form>
+		<form onsubmit={() => login()}>
 			<div data-mdb-input-init class="form-outline mb-4">
 				<label class="form-label" for="nutzername">Nutzername</label>
 				<input
 					type="text"
 					id="nutzername"
 					class="form-control form-control-lg"
-					placeholder="example_user"
+					placeholder="Beispielnutzer"
 					bind:value={username}
+					onkeydown={(event) => {
+						if (event.key == 'Enter') login();
+					}}
 				/>
 			</div>
 			<div data-mdb-input-init class="form-outline mb-3">
@@ -43,16 +57,17 @@
 					type="password"
 					id="pwform"
 					class="form-control form-control-lg"
-					placeholder=""
-					bind:value={password}
+					placeholder="Passwort"
+					bind:value={pw}
+					onkeydown={(event) => {
+						if (event.key == 'Enter') login();
+					}}
 				/>
 			</div>
 
 			<div class="text-center text-lg-start mt-4 pt-2">
 				<button
 					type="button"
-					data-mdb-button-init
-					data-mdb-ripple-init
 					class="btn btn-primary btn-lg"
 					style="padding-left: 2.5rem; padding-right: 2.5rem;"
 					onclick={login}
